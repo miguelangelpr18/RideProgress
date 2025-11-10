380d4ab9a4ee88c8abe93277e685244c839cd092
miguelangelpr18 <miguelangelpenar18@gmail.com>
2025-11-10 14:25:24 -0600

    rideprogresss

---
+++ RideProgress/RideProgress/RideProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideProgressApp.swift
+//  RideProgress
+//
+//  Created by Miguel Angel Peña Rivas  on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RideProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
